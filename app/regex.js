241a71f89da68491b4bb2fe7c1111330cf4b3edd
Ivan Khud <anakin16@gmail.com>
2017-10-11 14:41:24 +0300

    regex: containsNumber

--- app/regex.js
+++ app/regex.js
@@ -2,7 +2,15 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    var b = false, x;    
+    for (var i=0; i < str.length; i++)
+      {
+        x = parseInt(str[i]);
+        if (! isNaN(x)) {
+          b = true;
+        }
+      }
+    return b;
   },
 
   containsRepeatingLetter: function(str) {

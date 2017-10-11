fd1590a2a8e9d6d64e779a3f3ded40b5934a557a
Ivan Khud <anakin16@gmail.com>
2017-10-11 13:30:58 +0300

    Strings.js: reduceString

--- app/strings.js
+++ app/strings.js
@@ -1,8 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
 
+  reduceString: function(str, amount) {
+    var k=1, str1 = str[0];
+    for (i = 1; i < str.length; i++) {
+      if (str[i] === str[i-1]) {
+        if (k < amount) {
+          str1 += str[i];
+          k++;
+        }
+      }
+      else {
+        k=1;
+        str1 += str[i];
+      };
+    }
+    str = str1;
+    return str; 
   },
 
   wordWrap: function(str, cols) {
@@ -10,11 +25,12 @@ exports.stringsAnswers = {
   },
 
   reverseString: function(str) {
-    s1 = "";
+    var s1 = "";
     for (var i = str.length - 1; i >= 0; i--) {
       s1 += str[i];
     }
     str = s1; 
     return str;
   }
+
 };

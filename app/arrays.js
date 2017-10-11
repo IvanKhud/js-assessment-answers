3d7ffcd72d90ae2492c30a96d9bd97323a021884
Ivan Khud <anakin16@gmail.com>
2017-10-11 11:04:24 +0300

    Arrays.js: square

--- app/arrays.js
+++ app/arrays.js
@@ -98,7 +98,10 @@ exports.arraysAnswers = {
   },
 
   square: function(arr) {
-
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] *= arr[i];
+    }
+    return arr;
   },
 
   findAllOccurrences: function(arr, target) {

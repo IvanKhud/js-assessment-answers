f0f98c2ec961d7db72517371e76274ba089bc849
Ivan Khud <anakin16@gmail.com>
2017-10-16 13:08:52 +0300

    recursion: validParenthesses

--- app/recursion.js
+++ app/recursion.js
@@ -17,6 +17,20 @@ exports.recursionAnswers = {
   },
 
   validParentheses: function(n) {
+    if (n == 0)
+        return [""];
 
+    var result = [];
+    for (var i = 0; i < n; ++i) {
+
+        var lefts = exports.recursionAnswers.validParentheses(i);
+        var rights = exports.recursionAnswers.validParentheses(n - i - 1);
+
+        for (var l = 0; l < lefts.length; l++)
+            for (var r = 0; r < rights.length; r++)
+                result.push("(" + lefts[l] + ")" + rights[r]);
+    }
+
+    return result;
   }
 };
